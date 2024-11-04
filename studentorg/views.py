85ccbeeaa7076e18824779fa3a9e2e4a2815ee94
ArnejoKeinth <202280176@psu.palawan.edu.ph>
2024-11-04 19:53:11 +0800

    Updated and created views, forms, urls, org_add, and org_list

--- studentorg/views.py
+++ studentorg/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from studentorg.models import Organization
+from studentorg.forms import OrganizationForm
+from django.urls import reverse_lazy 
 template_name = "home.html"
 
 class OrganizationList(ListView):
@@ -13,3 +16,10 @@ class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
+    paginate_by = 5 
+
+class OrganizationCreateView(CreateView):
+     model = Organization
+     form_class = OrganizationForm
+     template_name = 'org_add.html'
+     success_url = reverse_lazy('organization-list') 
