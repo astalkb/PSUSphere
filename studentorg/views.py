33c05a3813cc167d69c54c46c8248d73e2fd2a61
ArnejoKeinth <202280176@psu.palawan.edu.ph>
2024-11-04 19:26:31 +0800

    create & modified url,views,base and org_list

--- studentorg/views.py
+++ studentorg/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from studentorg.models import Organization
+template_name = "home.html"
+
+class OrganizationList(ListView):
+     model = Organization
+     context_object_name = 'organization'
+     template_name = 'org_list.html'
+     paginate_by = 5
 
 class HomePageView(ListView):
     model = Organization
