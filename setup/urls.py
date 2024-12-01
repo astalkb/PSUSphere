e18077a1d5b415eb4095e9493abd71abad08049a
UzziKyle <uzzielkyleynciong0824@gmail.com>
2024-12-01 19:28:54 +0800

    Fix bugs

--- setup/urls.py
+++ setup/urls.py
@@ -17,7 +17,7 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 from django.contrib import admin
 from django.urls import path, re_path
 from studentorg.views import (
-    HomePageView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country, multipleBarbySeverity,
+    HomePageView,
     OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView,
     OrgMemberList, OrgMemberCreateView, OrgMemberUpdateView, OrgMemberDeleteView,
     StudentList, StudentCreateView, StudentUpdateView, StudentDeleteView,
@@ -30,58 +30,60 @@ from studentorg import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-     path("admin/", admin.site.urls),
-     path('', views.HomePageView.as_view(), name='home'),
-     path('chart/', PieCountbySeverity, name='chart'),
-     path('lineChart/', LineCountbyMonth, name='chart'),
-     path('multilineChart/', MultilineIncidentTop3Country, name='chart'),
-     path('multipleBarbySeverity/', multipleBarbySeverity, name='chart'),
-     path('radarChartOrgParticipation/', RadarChartOrgParticipation, name='radar-chart'),
-     path('bubbleChartStudentPrograms/', BubbleChartStudentPrograms, name='bubble-chart'),
-     path('horizontalBarTopOrgs/', HorizontalBarTopOrganizations, name='horizontal-bar-chart'),
-     path('stackedBarOrgMemberTrends/', StackedBarOrgMemberTrends, name='stacked-bar-chart'),
-     path('doughnutProgramDistribution/', DoughnutProgramDistribution, name='doughnut-chart'),
+    path("admin/", admin.site.urls),
+    path('', views.HomePageView.as_view(), name='home'),
+    path('radarChartOrgParticipation/',
+         RadarChartOrgParticipation, name='radar-chart'),
+    path('bubbleChartStudentPrograms/',
+         BubbleChartStudentPrograms, name='bubble-chart'),
+    path('horizontalBarTopOrgs/', HorizontalBarTopOrganizations,
+         name='horizontal-bar-chart'),
+    path('stackedBarOrgMemberTrends/',
+         StackedBarOrgMemberTrends, name='stacked-bar-chart'),
+    path('doughnutProgramDistribution/',
+         DoughnutProgramDistribution, name='doughnut-chart'),
 
-     # Organization
-     path('organization_list', OrganizationList.as_view(), name='organization-list'),
-     path('organization_list/add', OrganizationCreateView.as_view(),
-          name='organization-add'),
-     path('organization_list/<pk>', OrganizationUpdateView.as_view(),
-          name='organization-update'),
-     path('organization_list/<pk>/delete',
-          OrganizationDeleteView.as_view(), name='organization-delete'),
+    # Organization
+    path('organization_list', OrganizationList.as_view(),
+         name='organization-list'),
+    path('organization_list/add', OrganizationCreateView.as_view(),
+         name='organization-add'),
+    path('organization_list/<pk>', OrganizationUpdateView.as_view(),
+         name='organization-update'),
+    path('organization_list/<pk>/delete',
+         OrganizationDeleteView.as_view(), name='organization-delete'),
 
-     # OrgMember URLs
-     path('orgmember_list/', OrgMemberList.as_view(), name='orgmember-list'),
-     path('orgmember_list/add/', OrgMemberCreateView.as_view(), name='orgmember-add'),
-     path('orgmember_list/<pk>/', OrgMemberUpdateView.as_view(),
-          name='orgmember-update'),
-     path('orgmember_list/<pk>/delete/',
-          OrgMemberDeleteView.as_view(), name='orgmember-delete'),
+    # OrgMember URLs
+    path('orgmember_list/', OrgMemberList.as_view(), name='orgmember-list'),
+    path('orgmember_list/add/', OrgMemberCreateView.as_view(), name='orgmember-add'),
+    path('orgmember_list/<pk>/', OrgMemberUpdateView.as_view(),
+         name='orgmember-update'),
+    path('orgmember_list/<pk>/delete/',
+         OrgMemberDeleteView.as_view(), name='orgmember-delete'),
 
-     # Student URLs
-     path('student_list/', StudentList.as_view(), name='student-list'),
-     path('student_list/add/', StudentCreateView.as_view(), name='student-add'),
-     path('student_list/<pk>/', StudentUpdateView.as_view(), name='student-update'),
-     path('student_list/<pk>/delete/',
-          StudentDeleteView.as_view(), name='student-delete'),
+    # Student URLs
+    path('student_list/', StudentList.as_view(), name='student-list'),
+    path('student_list/add/', StudentCreateView.as_view(), name='student-add'),
+    path('student_list/<pk>/', StudentUpdateView.as_view(), name='student-update'),
+    path('student_list/<pk>/delete/',
+         StudentDeleteView.as_view(), name='student-delete'),
 
-     # College URLs
-     path('college_list/', CollegeList.as_view(), name='college-list'),
-     path('college_list/add/', CollegeCreateView.as_view(), name='college-add'),
-     path('college_list/<pk>/', CollegeUpdateView.as_view(), name='college-update'),
-     path('college_list/<pk>/delete/',
-          CollegeDeleteView.as_view(), name='college-delete'),
+    # College URLs
+    path('college_list/', CollegeList.as_view(), name='college-list'),
+    path('college_list/add/', CollegeCreateView.as_view(), name='college-add'),
+    path('college_list/<pk>/', CollegeUpdateView.as_view(), name='college-update'),
+    path('college_list/<pk>/delete/',
+         CollegeDeleteView.as_view(), name='college-delete'),
 
-     # Program URLs
-     path('program_list/', ProgramList.as_view(), name='program-list'),
-     path('program_list/add/', ProgramCreateView.as_view(), name='program-add'),
-     path('program_list/<pk>/', ProgramUpdateView.as_view(), name='program-update'),
-     path('program_list/<pk>/delete/',
-          ProgramDeleteView.as_view(), name='program-delete'),
+    # Program URLs
+    path('program_list/', ProgramList.as_view(), name='program-list'),
+    path('program_list/add/', ProgramCreateView.as_view(), name='program-add'),
+    path('program_list/<pk>/', ProgramUpdateView.as_view(), name='program-update'),
+    path('program_list/<pk>/delete/',
+         ProgramDeleteView.as_view(), name='program-delete'),
 
-     re_path(r'^login/$',
-               auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-     re_path(r'^logout/$',
-               auth_views.LogoutView.as_view(), name='logout'),
+    re_path(r'^login/$',
+            auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    re_path(r'^logout/$',
+            auth_views.LogoutView.as_view(), name='logout'),
 ]
